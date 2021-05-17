954257cc2d7612ecdfa4dffb9ab67bf74191f5b5
David Fernandez <david_fgdl@hotmail.com>
2021-05-17 10:23:42 +0100

    Actualizacion de categoria al editar categoria

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -14,36 +14,48 @@ export class CategoriesComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'actions'];
   dataSource = new MatTableDataSource();
+  oldCat: string;
+  newCat: string;
 
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(public categoryService: TaskService) { }
-  
+
   ngOnInit(): void {
     this.categoryService.getAllCategories().subscribe(res => this.dataSource.data = res);
   }
-  
+
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
 
-  onEdit(element){
+  onEdit(element) {
     this.categoryService.selectedCat = element;
+    this.oldCat = element.name;
   }
-  onDelete(id: string){
+  onDelete(id: string) {
     this.categoryService.deleteCategory(id);
   }
 
-  onSaveForm(){
-    if(this.categoryService.selectedCat.id == null){
+  onSaveForm() {
+    if (this.categoryService.selectedCat.id == null) {
       let newCat = {
         name: this.categoryService.selectedCat.name
       }
       this.categoryService.createCategory(newCat);
-    }else{
+    } else {
+      this.newCat = this.categoryService.selectedCat.name;
       this.categoryService.editCategory(this.categoryService.selectedCat);
+      (this.categoryService.tasks).forEach(tasks => {
+        tasks.forEach(task => {
+          if (task.category == this.oldCat) {
+            this.categoryService.selected = task;
+            this.categoryService.selected.category = this.newCat
+            this.categoryService.editTask(this.categoryService.selected);
+          }
+        })
+      })
     }
-    
     this.categoryService.selectedCat = {
       id: null,
       name: '',
